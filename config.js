c47509044e44769a02a6301e155153ececf309ab
{李鹏}({067014}) <sandon.lip@alibaba-inc.com>
2015-12-28 18:07:38 +0800

    to git

---
+++ config.js
@@ -0,0 +1,33 @@
+exports = module.exports = {
+    port: 80,
+	filters:[
+        //'host',
+		'rewrite'
+	],
+	hosts: {
+		// simple host
+		'www.demo.com': {
+			root: '/Users/Sandon/WebstormProjects/'
+		},
+
+		// rewrite
+		'www.styledemo.com': {
+			root: '/Users/Sandon/WebstormProjects/',
+			rewrite: [
+                {
+                    from: /^\/yugserverrewrite\/(.*)$/,
+                    to: 'mine/yug-server/test/$1'
+                },
+                {
+                    from: /^\/yugserverrewritehttp\/(.*)$/,
+                    to: 'http://www.demo.com/mine/yug-server/test/$1'
+                },
+                {
+                    from: '^(.*)$',
+                    to: 'http://42.156.140.62$1'
+                }
+            ]
+		}
+	}
+};
+
